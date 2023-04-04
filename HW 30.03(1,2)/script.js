8d57a5d6f9d6d9b9e8e1644372ef93e7493bfc0c
irnavi <118943206+irnavi@users.noreply.github.com>
2023-04-04 14:08:19 +0200

    HW ProFrontend

---
+++ HW 30.03(1,2)/script.js
@@ -0,0 +1,52 @@
+  // Задача 1:
+// Создайте в HTML 3 картинки без атрибутов. Используя следующий массив, задайте источник и альтернативный текст для каждой картинки.
+// [
+// {
+//     alt: "lion",
+//   src: "https://www.gesser-images.com/wp-content/uploads/2016/08/Lion-046-_DS30374-300x300.jpg"
+// },
+// {
+//     alt: "elephant",
+//   src: "https://globalelephants.org/wp-content/uploads/2014/05/Guida-Blog-Post-424-300x300.jpg"
+// },
+// {
+//     alt: "dog",
+//   src: "https://dickmanroadvet.com/wp-content/uploads/2016/07/dvm2-23-300x300.jpg"
+// },
+// ]
+
+
+
+const arrImg = [
+    {
+        alt: "lion",
+      src: "https://www.gesser-images.com/wp-content/uploads/2016/08/Lion-046-_DS30374-300x300.jpg"
+    },
+    {
+        alt: "elephant",
+      src: "https://globalelephants.org/wp-content/uploads/2014/05/Guida-Blog-Post-424-300x300.jpg"
+    },
+    {
+        alt: "dog",
+      src: "https://dickmanroadvet.com/wp-content/uploads/2016/07/dvm2-23-300x300.jpg"
+    },
+    ]
+
+const images = document.querySelectorAll('img');
+
+for(i = 0; i < arrImg.length; i++){
+    images[i].src = arrImg[i].src;
+    images[i].alt = arrImg[i].alt;
+}
+
+
+// Задача 2:
+// Создайте список с 3 пустыми пунктами в HTML.Используя следующий массив измените текст пунктов.
+// ['Item 1', 'Item 2', 'Item 3']
+
+const arrItem = ['Item 1', 'Item 2', 'Item 3'];
+
+const arrOfList = document.getElementsByTagName('li');
+for(let i = 0; i < arrItem.length; i++ ){
+  arrOfList[i].innerText = arrItem[i];
+}
